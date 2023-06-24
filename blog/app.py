6296b5f0716ca32ea530f214c2791edfa947df59
LAPTOP-RC7BFFPH\HP <zelaf44923@gmail.com>
2023-06-25 00:23:53 +0300

    feat: create the template of a post

--- blog/app.py
+++ blog/app.py
@@ -3,29 +3,37 @@ import sqlite3
 
 app = Flask(__name__)
 
-@app.route('/')
-
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
 
+def close_db_connection(conn):
+    conn.close()
+
+def init_db():
+    conn = get_db_connection()
+    conn.execute('CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, content TEXT NOT NULL)')
+    conn.close()
+
+@app.route('/')
 def index():
     conn = get_db_connection()
     posts = conn.execute('SELECT * FROM posts').fetchall()
     conn.close()
     return render_template('index.html', posts=posts)
 
-def init_db():
+@app.route('/<int:post_id>')
+def get_post(post_id):
     conn = get_db_connection()
-    conn.execute('CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, content TEXT NOT NULL)')
+    conn.execute('INSERT INTO posts (title, content) VALUES ("Random title", "Random text")')
+    post = conn.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
+    conn.close()
+    return render_template('post.html', post=post)
 
 @app.before_first_request
 def before_first_request():
     init_db()
-
-def close_db_connection(conn):
-    conn.close()
 
 if __name__ == '__main__':
     app.run()
