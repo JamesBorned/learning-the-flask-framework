e2531f278b7d6c35815d2be8506579b74968c4f0
LAPTOP-RC7BFFPH\HP <zelaf44923@gmail.com>
2023-06-24 23:17:33 +0300

    feat: connect database

--- blog/app.py
+++ blog/app.py
@@ -1,10 +1,31 @@
-from flask import Flask
-# экземпляр класса Flask
+from flask import Flask, render_template
+import sqlite3
+
 app = Flask(__name__)
-# задать маршрут во Flask
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def index():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
+
+def init_db():
+    conn = get_db_connection()
+    conn.execute('CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, content TEXT NOT NULL)')
+
+@app.before_first_request
+def before_first_request():
+    init_db()
+
+def close_db_connection(conn):
+    conn.close()
 
 if __name__ == '__main__':
     app.run()
