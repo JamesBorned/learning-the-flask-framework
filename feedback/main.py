3434f1c1d553169c6388f57949629b3189270426
LAPTOP-RC7BFFPH\HP <zelaf44923@gmail.com>
2023-07-14 00:50:54 +0300

    Add a layout of an example of a feedback form

---
+++ feedback/main.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, url_for, request
+
+app = Flask(__name__)
+
+menu = [{"name": "Установка", "url": "install-flask"},
+        {"name": "Первое приложение", "url": "first-app"},
+        {"name": "Обратная связь", "url": "contact"}]
+
+@app.route("/")
+def index():
+    print (url_for('index'))
+    return render_template('index.html', menu=menu)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title="О сайте", menu=menu)
+
+@app.route("/contact", methods=["POST", "GET"])
+def contact():
+    if request.method == 'POST':
+        print(request.form['message']) # объект request
+
+    return render_template('contact.html', title="Обратная связь", menu=menu)
+
+@app.route("/profile/<path:username>")  # int, float, path
+def profile(username):
+    return f"Пользователь: {username}"
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('about'))
+    print(url_for('profile', username="1234"))
+
+if __name__ == "__main__":
+    app.run(debug=True)
