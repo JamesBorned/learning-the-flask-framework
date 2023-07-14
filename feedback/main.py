c99c59fcd167979ed9f38ec8342b0d379bf70ade
LAPTOP-RC7BFFPH\HP <zelaf44923@gmail.com>
2023-07-15 00:53:01 +0300

    feat: add handlers of a request redirection and a request interruption

--- feedback/main.py
+++ feedback/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, flash
+from flask import Flask, render_template, url_for, request, flash, session, redirect, abort
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'fkfkfkfkfkfkfkfkffkfkf'
@@ -27,13 +27,26 @@ def contact():
 
     return render_template('contact.html', title="Обратная связь", menu=menu)
 
+@app.route("/profile/<username>")
+def profile(username):
+    if 'userLogged' not in session or session['userLogged'] != username:
+        abort(401)
+
+    return f"Пользователь: {username}"
+
+@app.route("/login", methods=["POST","GET"])
+def login():
+    if 'userLogged' in session:
+        return redirect(url_for('profile', username=session['userLogged']))
+    elif request.method == "POST" and request.form['username'] == "selfedu" and request.form['psw'] == "123":
+        session['userLogged'] = request.form['username']
+        return redirect(url_for('profile', username=session['userLogged']))
+
+    return render_template('login.html', title="Авторизация", menu=menu)
+
 @app.errorhandler(404) # декоратор
 def pageNotFound(error):
     return render_template('page404.html', title="Страница не найдена", menu=menu), 404
-
-@app.route("/profile/<path:username>")  # int, float, path
-def profile(username):
-    return f"Пользователь: {username}"
 
 # with app.test_request_context():
 #     print(url_for('index'))
