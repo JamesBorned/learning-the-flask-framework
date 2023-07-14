75479876bcd8863f6dd052d9a1f44bfd492ac530
LAPTOP-RC7BFFPH\HP <zelaf44923@gmail.com>
2023-07-14 22:18:45 +0300

    feat: add a form handler, a flash message about successful message sending

--- feedback/main.py
+++ feedback/main.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, flash
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'fkfkfkfkfkfkfkfkffkfkf'
 
 menu = [{"name": "Установка", "url": "install-flask"},
         {"name": "Первое приложение", "url": "first-app"},
@@ -18,18 +19,23 @@ def about():
 @app.route("/contact", methods=["POST", "GET"])
 def contact():
     if request.method == 'POST':
-        print(request.form['message']) # объект request
+        #print(request.form['message'])  объект request
+        if len(request.form['username']) > 2:
+            flash('Сообщение отправлено', category='success')
+        else:
+            flash('Ошибка отправки', category='error')
 
     return render_template('contact.html', title="Обратная связь", menu=menu)
+
 
 @app.route("/profile/<path:username>")  # int, float, path
 def profile(username):
     return f"Пользователь: {username}"
 
-with app.test_request_context():
-    print(url_for('index'))
-    print(url_for('about'))
-    print(url_for('profile', username="1234"))
+# with app.test_request_context():
+#     print(url_for('index'))
+#     print(url_for('about'))
+#     print(url_for('profile', username="1234"))
 
 if __name__ == "__main__":
     app.run(debug=True)
